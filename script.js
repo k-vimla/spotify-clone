820de7b96f54d3d8b3d0a7743726fcb94190eb9b
k-vimla <vimlakushwaha56@gmail.com>
2025-06-02 01:43:39 +0530

    there is no need of src as we are handling it via javascript

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 //Intialize the variables
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('song/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
 //audioElement.play();
+let songItems = Array.from(document.getElementsByClassName('songItem'));
 let songs= [
-    {songName:"SlySkyTelecasted", filePath:'song/1.mp3', coverPath:"coverPath/1.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/2.mp3', coverPath:"coverPath/2.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/3.mp3', coverPath:"coverPath/3.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/4.mp3', coverPath:"coverPath/4.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/5.mp3', coverPath:"coverPath/5.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/6.mp3', coverPath:"coverPath/6.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/7.mp3', coverPath:"coverPath/7.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/8.mp3', coverPath:"coverPath/8.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/9.mp3', coverPath:"coverPath/9.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/10.mp3', coverPath:"coverPath/10.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/11.mp3', coverPath:"coverPath/11.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/12.mp3', coverPath:"coverPath/12.jpg"},
-    {songName:"Salam-e-ishq", filePath:'song/13.mp3', coverPath:"coverPath/13.jpg"},
+    {songName:"Sly Sky - Telecasted", filePath:'song/1.mp3', coverPath:"coverPath/1.png"},
+    {songName:"Fireflies - The Mini Vandals", filePath:'song/2.mp3', coverPath:"coverPath/2.jpg"},
+    {songName:"Seagull - Telecasted", filePath:'song/3.mp3', coverPath:"coverPath/3.jpg"},
+    {songName:"Delayed Baggage - Ryan Stasik", filePath:'song/4.mp3', coverPath:"coverPath/4.jpg"},
+    {songName:"Future - Anno Domini Beats", filePath:'song/5.mp3', coverPath:"coverPath/5.jpeg"},
+    {songName:"Charm - Anno Domini Beats", filePath:'song/6.mp3', coverPath:"coverPath/6.jpg"},
+    {songName:"Window Shopping - Jeremy Black", filePath:'song/7.mp3', coverPath:"coverPath/7.jpeg"},
 ];
+
+songItems.forEach((element, i) => {
+    console.log(element, i);
+    element.getElementsByTagName('img')[0].src = songs[i].coverPath;
+    element.getElementsByClassName('songName')[0].innerText = songs[i].songName;
+
+})
 //hansdle paly pause click
+
+
+
 
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime <= 0){
@@ -44,4 +49,8 @@ audioElement.addEventListener('timeupdate', () =>{
     //console.log(progress)
     myProgressBar.value = progress;
 });
+
+myProgressBar.addEventListener('change', () =>{
+    audioElement.currentTime=myProgressBar.value * audioElement.duration/100;
+})
 
